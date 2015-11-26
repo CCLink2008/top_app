6d2f878634750c9e2aedc18e13588854ff9b11ff
Clink <clinkgas2006@hotmail.com>
2015-11-26 23:14:57 +0800

    model user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,35 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@user = User.new(name:"example user",email:"user@example.com")
+ 	
+  end
+   test "should be valid"do 
+   	 assert @user.valid?
+   end
+  test "name should be present" do 
+  	@user.name =" a   "*51
+  	assert_not @user.valid?
+  end 
+   test "name should not be too long" do 
+  	@user.name ="    "
+  	assert_not @user.valid?
+  end 
+  test "email should be present" do 
+  	@user.email ="    "
+  	assert_not @user.valid?
+  end 
+  test "email should not be too long" do 
+  	@user.email ="a    "*244 + "@example.com"
+  	assert_not @user.valid?
+  end 
+
+  tet "email address should be unique" do 
+     duplicate_user = @user.dup
+     duplicate_user.email = @user.email.upcase
+     @user.save 
+     assert_not duplicate_user.valid?
+  end 
 end
