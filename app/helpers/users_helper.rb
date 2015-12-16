a031c2580606bd93b5e877183c6537d1ca925e1f
Clink <clinkgas2006@hotmail.com>
2015-12-16 23:40:52 +0800

    fix some css errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-   def gravatar_for(user)
-   	 
+   def gravatar_for(user,options = { size:50 })   	 
    	 # gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
      # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-     gravatar_url = "http://i66.tinypic.com/vqmtg4.jpg"
+     size = options[:size]
+     gravatar_url = "http://i66.tinypic.com/vqmtg4.jpg?s=#{size}"
      image_tag(gravatar_url, alt: user.name, class: "gravatar")
    end
 end
