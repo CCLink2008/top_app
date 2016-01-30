ff8480999983ce69f31ce9af9b150bfcb0842b65
CCLink2008 <CCLink2008@users.noreply.github.com>
2016-01-30 11:29:16 +0800

    add fix
    add fix

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module TopApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
